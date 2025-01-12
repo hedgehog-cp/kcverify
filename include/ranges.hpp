82dfaedb160c80f24be331b77e0922f232f3dbf3
hedgehog <hedgehog.cpp@gmail.com>
2025-01-12 19:05:04 +0900

    🚧 change std alternatives

--- include/ranges.hpp
+++ include/ranges.hpp
@@ -1,17 +1,20 @@
-#ifndef KCVERIFY_CONCEPTS_HPP_INCLUDED
-#define KCVERIFY_CONCEPTS_HPP_INCLUDED
+#ifndef KCVERIFY_RANGES_HPP_INCLUDED
+#define KCVERIFY_RANGES_HPP_INCLUDED
 
 /**
- * @file concepts.hpp
- * @brief 標準ヘッダ<concepts>の拡張.
+ * @file ranges.hpp
+ * @brief 標準ヘッダ<ranges>の拡張.
  */
-#include <concepts>
+#include <ranges>
 
 // std
-#include <ranges>
-#include <type_traits>
+#include <concepts>
+
+// range-v3
+#include <range/v3/view/concat.hpp>
 
 namespace kcv {
+
 namespace ranges {
 
 /// @brief 要素型がValueである範囲Rangeを表すコンセプト.
@@ -21,17 +24,27 @@ namespace ranges {
 /// @see https://onihusube.hatenablog.com/entry/2022/10/09/021557#P1985R3-Universal-template-parameters
 /// @see https://stackoverflow.com/questions/65894358/how-to-concisely-express-the-c20-concept-of-range-containing-t-typed-values
 /// @note 標準入りするまで代替として利用する.
-template <class Range, class Value>
-concept range_of = std::ranges::range<Range> && std::is_same_v<Value, std::ranges::range_value_t<Range>>;
+template <typename Range, typename Value>
+concept range_of = std::ranges::range<Range> and std::same_as<Value, std::ranges::range_value_t<Range>>;
 
 /// @brief 要素型がValueであるビューViewを表すコンセプト.
 /// @tparam View ビュー
 /// @tparam Value ビューの要素型
 /// @note 標準入りするまで代替として利用する.
-template <class View, class Value>
-concept view_of = std::ranges::view<View> && std::is_same_v<Value, std::ranges::range_value_t<View>>;
+template <typename View, typename Value>
+concept view_of = std::ranges::view<View> and std::same_as<Value, std::ranges::range_value_t<View>>;
+
+namespace views {
+
+/// @note C++26のstd::ranges::views::concatの代替として, range-v3を使って実装.
+inline constexpr auto concat = ::ranges::views::concat;
+
+}  // namespace views
 
 }  // namespace ranges
+
+namespace views = kcv::ranges::views;
+
 }  // namespace kcv
 
-#endif  // KCVERIFY_CONCEPTS_HPP_INCLUDED
+#endif  // KCVERIFY_RANGES_HPP_INCLUDED
