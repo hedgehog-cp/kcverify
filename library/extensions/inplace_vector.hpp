973fb9fc7f4c9b5d2b990ee735b7e0dc366eba14
hedgehog <hedgehog.cpp@gmail.com>
2025-08-20 22:29:18 +0900

    🚧 add inplace_vector and interval tests

--- library/extensions/inplace_vector.hpp
+++ library/extensions/inplace_vector.hpp
@@ -3,161 +3,84 @@
 
 #include <cstddef>
 #include <iterator>
-#include <memory>
 #include <new>
-#include <stdexcept>
 
 namespace kcv {
 
-/// @brief 静的容量の可変長配列.
+/// @brief 静的容量の可変長配列. C++26のstd::inplace_vectorの代替.
 /// @tparam T 要素型.
 /// @tparam N キャパシティ.
-/// @note C++26のstd::inplace_vectorの代替.
-template <typename T, std::size_t C>
+template <typename T, std::size_t N>
 struct inplace_vector final {
-    using value_type             = T;
-    using size_type              = std::size_t;
-    using reference              = T&;
-    using const_reference        = const T&;
-    using pointer                = T*;
-    using const_pointer          = const T*;
-    using iterator               = T*;
-    using const_iterator         = const T*;
-    using reverse_iterator       = std::reverse_iterator<iterator>;
-    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
-    using difference_type        = std::ptrdiff_t;
+    using value_type      = T;
+    using size_type       = std::size_t;
+    using reference       = T&;
+    using const_reference = const T&;
+    using iterator        = T*;
+    using const_iterator  = const T*;
 
-    constexpr auto at(size_type n) -> reference {
-        if (n >= this->size_) {
-            throw std::out_of_range{"out of range."};
+    constexpr inplace_vector() noexcept = default;
+
+    constexpr inplace_vector(std::initializer_list<T> il) {
+        if (il.size() > N) {
+            throw std::bad_alloc{};
         }
 
-        return this->storage_[n];
-    }
-
-    constexpr auto at(size_type n) const -> const_reference {
-        if (n >= this->size_) {
-            throw std::out_of_range{"out of range."};
+        for (auto i = 0uz; const auto& e : il) {
+            this->data_[i++] = e;
         }
-
-        return this->storage_[n];
+        this->size_ = il.size();
     }
 
     constexpr auto operator[](size_type n) noexcept -> reference {
-        return this->storage_[n];
+        return this->data_[n];
     }
 
     constexpr auto operator[](size_type n) const noexcept -> const_reference {
-        return this->storage_[n];
-    }
-
-    constexpr auto front() noexcept -> reference {
-        return this->storage_[0];
-    }
-
-    constexpr auto front() const noexcept -> const_reference {
-        return this->storage_[0];
-    }
-
-    constexpr auto data() noexcept -> pointer {
-        return std::addressof(this->storage_[0]);
-    }
-
-    constexpr auto data() const noexcept -> const_pointer {
-        return std::addressof(this->storage_[0]);
+        return this->data_[n];
     }
 
     constexpr auto begin() noexcept -> iterator {
-        return std::addressof(this->storage_[0]);
+        return this->data_;
     }
 
     constexpr auto end() noexcept -> iterator {
-        return std::addressof(this->storage_[this->size_]);
+        return this->data_ + this->size_;
     }
 
     constexpr auto begin() const noexcept -> const_iterator {
-        return std::addressof(this->storage_[0]);
+        return this->data_;
     }
 
     constexpr auto end() const noexcept -> const_iterator {
-        return std::addressof(this->storage_[this->size_]);
-    }
-
-    constexpr auto cbegin() const noexcept -> const_iterator {
-        return std::addressof(this->storage_[0]);
-    }
-
-    constexpr auto cend() const noexcept -> const_iterator {
-        return std::addressof(this->storage_[this->size_]);
-    }
-
-    constexpr auto rbegin() noexcept -> reverse_iterator {
-        return std::addressof(this->storage_[0]);
-    }
-
-    constexpr auto rend() noexcept -> reverse_iterator {
-        return std::addressof(this->storage_[this->size_]);
-    }
-
-    constexpr auto rbegin() const noexcept -> const_reverse_iterator {
-        return std::addressof(this->storage_[0]);
-    }
-
-    constexpr auto rend() const noexcept -> const_reverse_iterator {
-        return std::addressof(this->storage_[this->size_]);
-    }
-
-    constexpr auto crbegin() const noexcept -> const_reverse_iterator {
-        return std::addressof(this->storage_[0]);
-    }
-
-    constexpr auto crend() const noexcept -> const_reverse_iterator {
-        return std::addressof(this->storage_[this->size_]);
-    }
-
-    constexpr bool empty() const noexcept {
-        return this->size_ == 0;
+        return this->data_ + this->size_;
     }
 
     constexpr auto size() const noexcept -> size_type {
         return this->size_;
     }
 
-    constexpr auto max_size() const noexcept -> size_type {
-        return C;
-    }
-
-    constexpr auto capacity() const noexcept -> size_type {
-        return C;
-    }
-
     constexpr void push_back(const value_type& v) {
-        if (this->size_ >= C) {
+        if (this->size_ == N) {
             throw std::bad_alloc{};
         }
 
-        this->storage_[this->size_] = v;
+        this->data_[this->size_] = v;
         this->size_++;
     }
 
     constexpr void push_back(value_type&& v) {
-        if (this->size_ >= C) {
+        if (this->size_ == N) {
             throw std::bad_alloc{};
         }
 
-        this->storage_[this->size_] = std::move(v);
+        this->data_[this->size_] = std::move(v);
         this->size_++;
     }
 
-    // constexpr void resize(size_type);
-    // constexpr void resize(size_type, const value_type&);
-    // constexpr void reserve(size_type n);
-    // constexpr void shrink_to_fit();
-    // constexpr void fill(const T&);
-    // constexpr void swap(inplace_vector&) noexcept(noexcept(swap(declval<T&>(), declval<T&>())));
-
-    size_type size_ = 0;
-    value_type storage_[C];
+   private:
+    size_type size_;
+    value_type data_[N];
 };
 
 }  // namespace kcv
---
+++ test/library/extensions/inplace_vector.cpp
@@ -0,0 +1,40 @@
+#include "extensions/inplace_vector.hpp"
+
+#include <cstdlib>
+#include <new>
+#include <ranges>
+#include <variant>
+
+int main() {
+    static_assert(std::ranges::contiguous_range<kcv::inplace_vector<std::monostate, 1uz>>);
+
+    {
+        constexpr auto vec = kcv::inplace_vector<int, 3>{0, 1, 2};
+        static_assert(vec.size() == 3uz);
+        static_assert(vec[0] == 0);
+        static_assert(vec[1] == 1);
+        static_assert(vec[2] == 2);
+    }
+
+    try {
+        const auto _ = kcv::inplace_vector<int, 2uz>{1, 2, 3};
+        return EXIT_FAILURE;
+    } catch (const std::bad_alloc &) {
+        // ok
+    }
+
+    {
+        constexpr auto vec = std::ranges::views::iota(0, 3) | std::ranges::to<kcv::inplace_vector<int, 3uz>>();
+        static_assert(vec.size() == 3uz);
+        static_assert(vec[0] == 0);
+        static_assert(vec[1] == 1);
+        static_assert(vec[2] == 2);
+    }
+
+    try {
+        const auto _ = std::ranges::views::iota(0, 3) | std::ranges::to<kcv::inplace_vector<int, 2uz>>();
+        return EXIT_FAILURE;
+    } catch (const std::bad_alloc &) {
+        // ok
+    }
+}
