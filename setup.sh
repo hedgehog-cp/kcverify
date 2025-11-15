84cd24db72f0914f8db5aaf0da793e03d46c7860
hedgehog <hedgehog.cpp@gmail.com>
2025-11-15 23:17:56 +0900

    🚧 add kc3kai bonus

--- setup.sh
+++ setup.sh
@@ -3,5 +3,34 @@
 # TODO: もっと安全な方法にする.
 curl https://raw.githubusercontent.com/Tibowl/api_start2/refs/heads/master/parsed/api_mst_ship.json             > ./assets/api_mst_ship.json
 curl https://raw.githubusercontent.com/Tibowl/api_start2/refs/heads/master/parsed/api_mst_slotitem.json         > ./assets/api_mst_slotitem.json
+
 curl https://raw.githubusercontent.com/ElectronicObserverEN/Data/refs/heads/master/Data/FitBonuses.json         > ./assets/fit_bonuses.json
-curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/mst_slotitem_bonus.json > ./assets/mst_slotitem_bonus.json
+
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/mst_slotitem_bonus.json > ./assets/kc3kai/mst_slotitem_bonus.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/57-1.json         > ./assets/kc3kai/bonus/57-1.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/57-2.json         > ./assets/kc3kai/bonus/57-2.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/57-3.json         > ./assets/kc3kai/bonus/57-3.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/57-4.json         > ./assets/kc3kai/bonus/57-4.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/57-5.json         > ./assets/kc3kai/bonus/57-5.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/57-6.json         > ./assets/kc3kai/bonus/57-6.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/57-7.json         > ./assets/kc3kai/bonus/57-7.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/58-1.json         > ./assets/kc3kai/bonus/58-1.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/58-2.json         > ./assets/kc3kai/bonus/58-2.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/58-3.json         > ./assets/kc3kai/bonus/58-3.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/58-4.json         > ./assets/kc3kai/bonus/58-4.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/59-1.json         > ./assets/kc3kai/bonus/59-1.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/59-2.json         > ./assets/kc3kai/bonus/59-2.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/59-3.json         > ./assets/kc3kai/bonus/59-3.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/59-4.json         > ./assets/kc3kai/bonus/59-4.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/59-5.json         > ./assets/kc3kai/bonus/59-5.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/60-1.json         > ./assets/kc3kai/bonus/60-1.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/60-2.json         > ./assets/kc3kai/bonus/60-2.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/60-3.json         > ./assets/kc3kai/bonus/60-3.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/60-4.json         > ./assets/kc3kai/bonus/60-4.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/60-5.json         > ./assets/kc3kai/bonus/60-5.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/60-6.json         > ./assets/kc3kai/bonus/60-6.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/61-1.json         > ./assets/kc3kai/bonus/61-1.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/61-2.json         > ./assets/kc3kai/bonus/61-2.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/61-3.json         > ./assets/kc3kai/bonus/61-3.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/7-4.json          > ./assets/kc3kai/bonus/7-4.json
+curl https://raw.githubusercontent.com/KC3Kai/kancolle-replay/refs/heads/master/js/data/bonus/7-5.json          > ./assets/kc3kai/bonus/7-5.json
