91433d9c418feba9b371888dd974bf4a6780d77a
hedgehog <hedgehog.cpp@gmail.com>
2026-01-28 01:22:55 +0900

    🚧 move verify_damage_formula

--- library/include/kcv/domain/verification/damage_formula/damage_formula_verification.hpp
+++ library/include/kcv/domain/verification/damage_formula/damage_formula_verification.hpp
@@ -1,83 +1,149 @@
 #ifndef KCV_DOMAIN_VERIFICATION_DAMAGE_FORMULA_DAMAGE_FORMULA_VERIFICATION_HPP_INCLUDED
 #define KCV_DOMAIN_VERIFICATION_DAMAGE_FORMULA_DAMAGE_FORMULA_VERIFICATION_HPP_INCLUDED
 
-#include <optional>
-#include <vector>
+// std
+#include <iostream>
+#include <ostream>
+#include <utility>
 
+// kcv
 #include "kcv/core/context_data.hpp"
 #include "kcv/domain/verification/battlelog/battlelog.hpp"
-#include "kcv/domain/verification/damage_formula/damage_formula.hpp"
-#include "kcv/domain/verification/damage_formula/inverse_formula.hpp"
 
 namespace kcv {
 
-/// @brief ダメージ説明結果.
-struct explain_damage_result final {
-    /// @brief ダメージ式.
-    kcv::damage_formula damage_formula;
+/// @brief kcv::verify_damage_formula関数の書き出し操作を制御する.
+/// メンバがtrueのとき, その項目の計算結果を書き出す.
+struct vdf_output_policy final {
+    // -------------------------------------------------------------------------
+    // 順算:
+    //   順向きのダメージ計算.
+    // -------------------------------------------------------------------------
 
-    /// @brief 逆算可能判定.
-    bool is_invertible_damage_formula;
+    /// @brief 順算: 基本攻撃力を書き出す.
+    bool base_attack_power = true;
 
-    /// @brief 説明可能判定.
-    bool explainable_damage;
+    /// @brief 順算: 交戦形態補正を書き出す.
+    bool engagement = true;
 
-    /// @brief 逆算結果 攻撃力式.
-    std::optional<kcv::inverse_attack_formula_result_t> inversed_attack_modifiers;
+    /// @brief 順算: 攻撃側陣形補正を書き出す.
+    bool formation = true;
 
-    /// @brief 逆算結果 防御力式.
-    std::optional<kcv::inverse_defence_formula_result_t> inversed_defence_modifiers;
+    /// @brief 順算: 損傷状態補正を書き出す.
+    bool damage_state = true;
+
+    /// @brief 順算: 前対潜シナジー補正を書き出す.
+    bool pre_asw = true;
+
+    /// @brief 順算: 後対潜シナジー補正を書き出す.
+    bool post_asw = true;
+
+    /// @brief 順算: 装甲減少補正を書き出す.
+    bool ap_depth_charge = true;
+
+    /// @brief 順算: 最終攻撃力を書き出す.
+    bool attack_power = true;
+
+    /// @brief 順算: 最終防御力を書き出す.
+    bool defence_power = true;
+
+    /// @brief 順算: ダメージを書き出す.
+    bool damage = true;
+
+    // -------------------------------------------------------------------------
+    // 逆算:
+    //   観測したダメージおよびダメージ式を用いて各補正値を解く計算.
+    // -------------------------------------------------------------------------
+
+    /// @brief 逆算: 未知の第0種補正.
+    /// firstがtrueのとき各値を書き出す. secondがtrueのとき集計値を書き出す.
+    std::pair<bool, bool> inversed_f0 = {true, true};
+
+    /// @brief 逆算: 未知の第14種補正.
+    /// firstがtrueのとき各値を書き出す. secondがtrueのとき集計値を書き出す.
+    std::pair<bool, bool> inversed_f14 = {true, true};
+
+    /// @brief 逆算: 未知の第3種補正.
+    /// firstがtrueのとき各値を書き出す. secondがtrueのとき集計値を書き出す.
+    std::pair<bool, bool> inversed_f3 = {true, true};
+
+    /// @brief 逆算: 未知の第2種補正.
+    /// firstがtrueのとき各値を書き出す. secondがtrueのとき集計値を書き出す.
+    std::pair<bool, bool> inversed_f2 = {true, true};
 };
 
-/// @brief 観測したダメージを, その状況化でのダメージ式で説明する.
-auto explain_damage(const kcv::context_data& ctx, const kcv::battlelogs_t& battlelogs)
-    -> std::vector<explain_damage_result>;
+/// @brief ダメージ式を検証する.
+/// @param ctx マスタを含む背景情報.
+/// @param battlelogs 検証対象の全戦闘ログ.
+/// @param output_policy 書き出し制御ポリシー.
+/// @param os 検証結果の書き出し先.
+/// @details 検証対象の全戦闘ログそれぞれに対してダメージ式を立式･評価し, 観測されたダメージの説明可能性を検証する.
+/// 逆算可能である場合は, 観測ダメージおよびダメージ式から補正値の逆算を行う.
+/// 検証結果を`output_policy`に従って`os`に書き出す.
+void verify_damage_formula(
+    const kcv::context_data& ctx,
+    const kcv::battlelogs_t& battlelogs,
+    const kcv::vdf_output_policy& output_policy = {},
+    std::ostream& os                            = std::cout
+);
 
 /// @brief 逆算可能判定.
+/// 理論上または実装上, ダメージ式の各補正値を逆算できるかを検証する. 逆算可能ならばtrueを返す.
+/// この関数は`kcv::verify_damage_formula`の内部で呼ばれる.
+/// @details 以下の判定いずれにおいても偽であるならば逆算可能.
+/// @see `is_miss`
+/// @see `is_scratch_damage`
+/// @see `is_lethal_replacement_damage`
+/// @see     `is_lethal_replaced_damage_on_normal_condition`
+/// @see     `is_lethal_replaced_damage_on_very_tired`
+/// @see     `is_lethal_replaced_damage_by_friendly_attack`
+/// @see `is_night_air_attack_ci_type_indeterminable`
+/// @see `is_fba_combined_critical_air_attack`
+/// @see `is_undefined_slot_num_air_attack`
 bool is_invertible_damage_formula(const kcv::context_data& ctx, const kcv::battlelog& data);
 
 /// @brief miss判定.
+/// missのとき, ダメージは0または割合置換となるため逆算できない.
 bool is_miss(const kcv::context_data& ctx, const kcv::battlelog& data);
 
 /// @brief 割合置換判定.
+/// 割合置換のとき, 攻撃力および防御力によらないダメージに置換されるため逆算できない.
 bool is_scratch_damage(const kcv::context_data& ctx, const kcv::battlelog& data);
 
 /// @brief 致死置換判定. 第1種致死置換, 第2種致死置換, 第3種致死置換のいずれかであるかを検証する.
+/// 致死置換のとき, 攻撃力および防御力によらないダメージに置換されるため逆算できない.
 bool is_lethal_replacement_damage(const kcv::context_data& ctx, const kcv::battlelog& data);
 
-/// @brief 第1種致死置換判定. このとき, [floor(hp*0.5), floor(hp*0.5)+floor((1-eps)*hp)*0.3]に置換される.
+/// @brief 第1種致死置換判定.
+/// 第1種致死置換のとき, ダメージが[floor(hp*0.5), floor(hp*0.5+max(hp-1,0)*0.3)]に置換される.
 bool is_lethal_replaced_damage_on_normal_condition(const kcv::context_data& ctx, const kcv::battlelog& data);
 
-/// @brief 第2種致死置換判定. 赤疲労致死置換. このとき, hp-1に置換される.
+/// @brief 第2種致死置換判定.
+/// 第2種致死置換のとき, ダメージがhp-1に置換される.
 bool is_lethal_replaced_damage_on_very_tired(const kcv::context_data& ctx, const kcv::battlelog& data);
 
-/// @brief 第3種致死置換判定. 友軍艦隊対深海棲艦終局阻止生存保証. このとき, floor(hp*0.5)に置換される.
+/// @brief 第3種致死置換判定
+/// 第3種致死置換のとき, ダメージがfloor(hp*0.5)に置換される.
 bool is_lethal_replaced_damage_by_friendly_attack(const kcv::context_data& ctx, const kcv::battlelog& data);
 
-//-------------
-
 /// @brief 夜襲CI種別判別不可能判定.
+/// 夜襲CIの種別を表示装備などから判別できないため攻撃力を決定できない.
 bool is_night_air_attack_ci_type_indeterminable(const kcv::context_data& ctx, const kcv::battlelog& data);
 
 /// @brief 戦爆連合熟練急所弾判定.
+/// 空母によるFBAなどの戦爆連合の攻撃において熟練急所弾の補正値が未詳であるため攻撃力を決定できない.
 bool is_fba_combined_critical_air_attack(const kcv::context_data& ctx, const kcv::battlelog& data);
 
-/// @brief 攻撃艦轟沈判定. 不要.
-/// @brief 防御艦轟沈判定. 不要.
-
-/// @brief 攻撃種別判定. おそらくこの判定自体を定義できない.
-bool is_undefined_attack(const kcv::context_data& ctx, const kcv::battlelog& data);
-
 /// @brief 敵空母戦闘後搭載数不明攻撃判定.
-bool is_undefined_slot_num_air_attack(const kcv::context_data& ctx, const kcv::battlelog& data);
+/// ある時期以降に追加された深海棲艦の各スロットの搭載数が不明であるため攻撃力を決定できない.
+bool is_unknown_slot_air_attack(const kcv::context_data& ctx, const kcv::battlelog& data);
 
-/// @brief 説明可能判定
-/// 逆算可能判定に依存する.
-// bool explainable_damage(const kcv::context_data& ctx, const kcv::battlelog& data);
-
-/// @brief 理論範囲内判定.
-// 観測ダメージと理論ダメージを引数にとるので、このシグネチャでは大きすぎる.
-// bool is_theoretical_damage(const kcv::context_data& ctx, const kcv::battlelog& data);
+// ダメージ検証用スプレで扱う以下の判定は, 未使用または定義できないため未定義.
+//   `攻撃艦轟沈判定`
+//   `防御艦轟沈判定`
+//   `攻撃種別判定`
+// ダメージ検証用スプレで扱う以下の判定は, 実装上の都合で関数化していない.
+//   `説明可能判定`
 
 }  // namespace kcv
 
--- source/main.cpp
+++ source/main.cpp
@@ -1,36 +1,21 @@
 // std
-#include <algorithm>
-#include <array>
 #include <cassert>
-#include <cstdio>
 #include <cstdlib>
 #include <exception>
 #include <filesystem>
-#include <limits>
-#include <optional>
 #include <print>
-#include <ranges>
 #include <stacktrace>
-#include <tuple>
-#include <type_traits>
-#include <utility>
 #include <vector>
 
 // kcv
 #include "kcv/core/constants/ship_attributes.hpp"
 #include "kcv/core/context_data.hpp"
 #include "kcv/core/json/read_json.hpp"
-#include "kcv/core/numeric/interval.hpp"
-#include "kcv/core/numeric/interval/basic_interval.hpp"
 #include "kcv/domain/verification/battlelog/battlelog.hpp"
 #include "kcv/domain/verification/battlelog/battlelog_accessor.hpp"
 #include "kcv/domain/verification/battlelog/battlelog_io.hpp"
-#include "kcv/domain/verification/damage_formula/damage_formula.hpp"
 #include "kcv/domain/verification/damage_formula/damage_formula_verification.hpp"
-#include "kcv/domain/verification/damage_formula/inverse_formula.hpp"
-#include "kcv/domain/verification/damage_formula/modifier_functions.hpp"
 #include "kcv/external/eoen/database/sortie/sortie_record.hpp"
-#include "kcv/external/kcsapi/extensions/damage_state.hpp"
 #include "kcv/std_ext/exception.hpp"
 #include "kcv/std_ext/formatter.hpp"
 
@@ -38,11 +23,6 @@
 /// @param data 戦闘ログ1件.
 /// @todo 条件を記述した設定ファイルを読み込んで, あるいはコマンドラインから条件を読み込んで, 条件の変更に対して再コンパイルを不要にする.
 bool macthes_battlelog(const kcv::battlelog& data);
-
-/// @brief ダメージ式を検証する.
-/// @param ctx マスタを含む背景情報.
-/// @param battlelogs 検証対象の全戦闘ログ.
-void verify_damage_formula(const kcv::context_data& ctx, const kcv::battlelogs_t& battlelogs);
 
 int main() try {
     std::set_terminate([]() static -> void { std::println(stderr, "fatal error: {}", std::stacktrace{}); });
@@ -77,7 +57,7 @@ int main() try {
     kcv::write_akakari(battlelogs, ctx.api_mst_ship(), ctx.api_mst_slotitem(), "./out/battlelogs.csv");
 
     // ダメージ式を検証する.
-    ::verify_damage_formula(ctx, battlelogs);
+    kcv::verify_damage_formula(ctx, battlelogs);
 } catch (const std::exception& e) {
     // 捕捉した例外をフォーマットして標準エラー出力に出力するとともに, エラーとしてプログラムを終了する.
     kcv::print_exception(stderr, e);
@@ -96,14 +76,13 @@ bool macthes_battlelog(const kcv::battlelog& data) {
     }
 
     // 深海棲艦の攻撃を除外.
-    if (data.attacker_side == kcv::kcsapi::fleet_flag::enemy) {
-        return false;
-    }
+    // if (data.attacker_side == kcv::kcsapi::fleet_flag::enemy) {
+    //     return false;
+    // }
 
-    // 対潜でない攻撃を除外.
+    // 夜戦でない攻撃を除外.
     // static constexpr auto target_phases = std::to_array<kcv::phase>({
-    //     kcv::phase::opening_atack,
-    //     kcv::phase::raigeki,
+    //     kcv::phase::midnight,
     // });
     // if (not std::ranges::contains(target_phases, data.phase)) {
     //     return false;
@@ -134,243 +113,4 @@ bool macthes_battlelog(const kcv::battlelog& data) {
     // }
 
     return true;
-}
-
-struct verify_damage_formula_result final {
-    /// @brief ダメージ式.
-    kcv::damage_formula damage_formula;
-
-    /// @brief 逆算可能判定.
-    bool is_invertible_damage_formula;
-
-    /// @brief 逆算結果 攻撃力補正値.
-    std::optional<kcv::inverse_attack_formula_result_t> inversed_attack_power_modifiers;
-
-    /// @brief 逆算結果 防御力補正値.
-    std::optional<kcv::inverse_defence_formula_result_t> inversed_defence_power_modifiers;
-
-    /// @brief ダメージ.
-    std::optional<kcv::number> damage;
-
-    /// @brief 説明可能判定.
-    bool is_explainable_damage;
-};
-
-/// @brief ダメージ式を標準出力に出力する.
-void print_damage_formula(const std::vector<verify_damage_formula_result>& results) {
-    for (const auto& result : results) {
-        const auto& atk_mods = result.damage_formula.attack_power_formula().modifier_function().decompose();
-        const auto& def_mods = result.damage_formula.defence_power_formula().modifier_function().decompose();
-
-        // 基本攻撃力.
-        const auto base_attack = result.damage_formula.attack_power_formula().base_value();
-        std::print("基本攻撃力: {:.3f}, ", base_attack);
-
-        // 第0種補正.
-        // const auto& [a0, b0] = std::get<kcv::functions::f0>(atk_mods);
-        // std::print("a0: {:.3f}, b0: {:.3f}, ", a0, b0);
-
-        // 交戦形態補正.
-        const auto& [a_engagement, _engagement] = std::get<kcv::functions::engagement>(atk_mods);
-        std::print("交戦形態補正: {:.3f}, ", a_engagement);
-
-        // 攻撃側陣形補正.
-        const auto& [a_formation, _formation] = std::get<kcv::functions::formation>(atk_mods);
-        std::print("攻撃側陣形補正: {:.3f}, ", a_formation);
-
-        // 損傷状態補正.
-        const auto& [a_damage_state, _damage_state] = std::get<kcv::functions::damage_state>(atk_mods);
-        std::print("損傷状態補正: {:.3f}, ", a_damage_state);
-
-        // 前対潜シナジー補正.
-        const auto& [a_pre, _pre] = std::get<kcv::functions::pre_asw>(atk_mods);
-        std::print("前対潜シナジー補正: {:.3f}, ", a_pre);
-
-        // 後対潜シナジー補正.
-        const auto& [a_post, _post] = std::get<kcv::functions::post_asw>(atk_mods);
-        std::print("後対潜シナジー補正: {:.3f}, ", a_post);
-
-        // 第14種補正
-        // const auto& [a14, b14] = std::get<kcv::functions::f14>(atk_mods);
-        // std::print("a14: {:.3f}, b14: {:.3f}, ", a14, b14);
-
-        // 第3種補正.
-        // const auto& [a3, b3] = std::get<kcv::functions::f3>(def_mods);
-        // std::print("a3: {:.3f}, b3: {:.3f}, ", a3, b3);
-
-        // 装甲減少補正.
-        const auto& [_ap_depth, b_ap_depth] = std::get<kcv::functions::depth_charge_armor_break>(def_mods);
-        std::print("装甲減少補正: {:.3f}, ", b_ap_depth);
-
-        // 攻撃力.
-        const auto& atk = result.damage_formula.attack_power();
-        std::print("最終攻撃力: {:.3f}, ", atk);
-
-        // 防御力.
-        const auto& def = result.damage_formula.defence_power();
-        std::print("最終防御力: {:.3f}, ", def);
-
-        // ダメージ.
-        const auto& dmg = result.damage;
-        std::print("理論ダメージ: {}, ", dmg);
-
-        std::println("");
-    }
-}
-
-/// @brief 逆算した結果を要約して標準出力に出力する.
-void print_summary(const kcv::battlelogs_t& battlelogs, const std::vector<verify_damage_formula_result>& results) {
-    std::println("入力件数: {}", results.size());
-
-    const auto inverse_target_num = std::ranges::count_if(results, [](const auto& e) static noexcept -> bool {  //
-        return e.is_invertible_damage_formula;
-    });
-    std::println("逆算対象: {}", inverse_target_num);
-
-    std::println("逆算不可能: {}", results.size() - inverse_target_num);
-
-    const auto greater_than_observed_damage = std::ranges::count_if(
-        std::ranges::views::zip(battlelogs, results),  //
-        [](const auto& e) static -> bool {
-            const auto& [battlelog, inversed] = e;
-            return inversed.damage.has_value() and battlelog.damage < *inversed.damage;
-        }
-    );
-    std::println("<理論最小値: {}", greater_than_observed_damage);
-
-    const auto in = std::ranges::count_if(
-        std::ranges::views::zip(battlelogs, results),  //
-        [](const auto& e) static -> bool {
-            const auto& [battlelog, inversed] = e;
-            return inversed.damage.has_value() and kcv::in(battlelog.damage, *inversed.damage);
-        }
-    );
-    std::println("範囲内: {}", in);
-
-    const auto less_than_observed_damage = std::ranges::count_if(
-        std::ranges::views::zip(battlelogs, results),  //
-        [](const auto& e) static -> bool {
-            const auto& [battlelog, inversed] = e;
-            return inversed.damage.has_value() and battlelog.damage < *inversed.damage;
-        }
-    );
-    std::println("理論最大値<: {}", less_than_observed_damage);
-
-    const auto equal_to_min = std::ranges::count_if(
-        std::ranges::views::zip(battlelogs, results),  //
-        [](const auto& e) static -> bool {
-            const auto& [battlelog, inversed] = e;
-            return inversed.damage.has_value() and battlelog.damage == inversed.damage->lower();
-        }
-    );
-    std::println("=理論最小値: {}", equal_to_min);
-
-    const auto equal_to_max = std::ranges::count_if(
-        std::ranges::views::zip(battlelogs, results),  //
-        [](const auto& e) static -> bool {
-            const auto& [battlelog, inversed] = e;
-            return inversed.damage.has_value() and battlelog.damage == inversed.damage->upper();
-        }
-    );
-    std::println("理論最大値=: {}", equal_to_max);
-
-    const auto explainable_damage_num = std::ranges::count_if(results, [](const auto& e) static -> bool {  //
-        return e.is_explainable_damage;
-    });
-    std::println("説明可能判定: {}", explainable_damage_num);
-}
-
-/// @brief 逆算結果のうち型Tのものを集計する.
-/// @tparam T 強い型付けをした線形補正関数. Tは逆算結果の型のうち唯一のものでなければならない.
-/// 例: T = kcv::functions::basic_liner<foo_tag>.
-/// @param inversed_results 逆算結果.
-/// @return 乗算補正値の最小値および上限, 加算補正値の最小値および上限.
-template <typename T>
-    requires kcv::functions::is_basic_liner_v<T>
-auto aggregate_attack_power_modifier(const std::vector<verify_damage_formula_result>& inversed_results) {
-    // 取り出す逆算結果の型.
-    using inversed_modifier_t = std::optional<kcv::inverse_result<T>>;
-
-    auto a_min = -std::numeric_limits<kcv::interval::base_type>::infinity();
-    auto a_sup = +std::numeric_limits<kcv::interval::base_type>::infinity();
-    auto b_min = -std::numeric_limits<kcv::interval::base_type>::infinity();
-    auto b_sup = +std::numeric_limits<kcv::interval::base_type>::infinity();
-
-    for (const auto& result : inversed_results) {
-        if (result.inversed_attack_power_modifiers.has_value()) {
-            // 逆算結果から型Tのものを取り出す.
-            const auto& inversed_modifier = std::get<inversed_modifier_t>(*result.inversed_attack_power_modifiers);
-            if (inversed_modifier.has_value()) {
-                // 逆算した乗算補正値a, 加算補正値bそれぞれについて, 補正値の共通区間(積集合)を求める.
-                // 最小値と上限との大小関係が逆算する場合があるため区間型を用いない.
-                const auto& [a, b] = *inversed_modifier;
-                if (a.has_value()) {
-                    a_min = std::max(a->lower(), a_min);
-                    a_sup = std::min(a->upper(), a_sup);
-                }
-                if (b.has_value()) {
-                    b_min = std::max(b->lower(), b_min);
-                    b_sup = std::min(b->upper(), b_sup);
-                }
-            }
-        }
-    }
-
-    return std::make_tuple(a_min, a_sup, b_min, b_sup);
-}
-
-/// @brief 逆算した補正値を標準出力に出力する.
-void print_modifier(const std::vector<verify_damage_formula_result>& results) {
-    const auto& [a0_min, a0_sup, b0_min, b0_sup] = aggregate_attack_power_modifier<kcv::functions::f0>(results);
-    std::println("第0種補正: {:.5f} ≦ a0 < {:.5f}, {:.5f} ≦ b0 < {:.5f}", a0_min, a0_sup, b0_min, b0_sup);
-
-    const auto& [a14_min, a14_sup, b14_min, b14_sup] = aggregate_attack_power_modifier<kcv::functions::f14>(results);
-    std::println("第14種補正: {:.5f} ≦ a14 < {:.5f}, {:.5f} ≦ b14 < {:.5f}", a14_min, a14_sup, b14_min, b14_sup);
-}
-
-void verify_damage_formula(const kcv::context_data& ctx, const kcv::battlelogs_t& battlelogs) {
-    auto results = std::vector<verify_damage_formula_result>{};
-    results.reserve(battlelogs.size());
-
-    for (const auto& data : battlelogs) {
-        // ダメージ式を立式.
-        auto damage_formula = kcv::formulate_damage(ctx, data);
-
-        // 逆算可能判定.
-        const bool is_invertible_damage_formula = kcv::is_invertible_damage_formula(ctx, data);
-        // 逆算.
-        auto inv_atk_pow_mods = is_invertible_damage_formula
-                                  ? std::make_optional(kcv::inverse_attack_power_formula(data.damage, damage_formula))
-                                  : std::nullopt;
-        auto inv_def_pow_mods = is_invertible_damage_formula
-                                  ? std::make_optional(kcv::inverse_defence_power_formula(data.damage, damage_formula))
-                                  : std::nullopt;
-
-        // ダメージ式を評価.
-        const auto damage = damage_formula.evaluate();
-        // 理論範囲内判定.
-        const bool is_theoretical_damage = damage.has_value() and kcv::in(data.damage, *damage);
-        // 説明可能判定 (異常ダメージ検知).
-        const bool is_explainable_damage = is_theoretical_damage or not is_invertible_damage_formula;
-
-        results.push_back(
-            verify_damage_formula_result{
-                .damage_formula                   = std::move(damage_formula),
-                .is_invertible_damage_formula     = is_invertible_damage_formula,
-                .inversed_attack_power_modifiers  = std::move(inv_atk_pow_mods),
-                .inversed_defence_power_modifiers = std::move(inv_def_pow_mods),
-                .damage                           = damage,
-                .is_explainable_damage            = is_explainable_damage,
-            }
-        );
-    }
-
-    assert(battlelogs.size() == results.size());
-
-    // 逆算結果を表示.
-    ::print_damage_formula(results);
-    std::println("------------");
-    ::print_summary(battlelogs, results);
-    std::println("------------");
-    ::print_modifier(results);
 }
